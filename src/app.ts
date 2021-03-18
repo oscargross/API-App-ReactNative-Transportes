842a3c996ac06314a9ac63445a38c6bb54b77238
Jean Santos <jean.santos@compasso.com.br>
2021-03-17 21:59:30 -0300

    Fix app log

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ export const InitApp = (routes: any) => {
     app.use(express.json());
     app.use(routes);
     app.listen(process.env.PORT, () => {
-        console.info(`Listenning at http://localhost: ${process.env.PORT}`);
+        console.info(`Listenning at http://localhost:${process.env.PORT}`);
       })
       .on('error', err => {
         console.info(`Err: Error listen server: ${err}`);
