371c21f841b63caf34a61dddf32df68f431019d7
Jean Santos <jean.santos@compasso.com.br>
2021-03-18 21:31:30 -0300

    Add boom lib and update error handling driver controller

--- src/controllers/DriverController.ts
+++ src/controllers/DriverController.ts
@@ -1,33 +1,52 @@
+import Boom from '@hapi/boom';
 import { Request, Response } from 'express'
 import  * as DriverService from '../services/AccessDBService'
 import driverSchema from '../schema/driverSchema';
 
 
 export let createDriver = async(req: Request, res: Response) => {
-    let driver = req.body;
-    res.send (await DriverService.create (driver, driverSchema))  
+    const { body: driver } = req
+    return DriverService.create(driver, driverSchema)
+        .then(result => res.status(201).json(result))
+        .catch(error => 
+            error.statusCode ? res.status(error.statusCode).json(error) : 
+            res.status(500).json(Boom.internal('Internal Server Error').output.payload))
 };
 
 export let readAllDrivers = async (req: Request, res: Response)=>  {
-    res.send(await DriverService.readAll(driverSchema))
-   
+    return DriverService.readAll(driverSchema)
+        .then(result => res.status(200).json(result))
+        .catch(error => 
+            error.statusCode ? res.status(error.statusCode).json(error) : 
+            res.status(500).json(Boom.internal('Internal Server Error').output.payload))
 };
 
-export let findDriverByParam = async (req: Request, res: Response)=>  {
-    let param = req.params.param
-    res.send(await DriverService.findByParam(param, driverSchema))
-   
+export let findDriverByParam = async (req: Request, res: Response) =>  {
+    const { id } = req.params
+    return DriverService.findByParam(id, driverSchema)
+    .then(result => res.status(200).json(result))
+    .catch(error => 
+        error.statusCode ? res.status(error.statusCode).json(error) : 
+        res.status(500).json(Boom.internal('Internal Server Error').output.payload))
 };
 
 export let updateDriver = async (req: Request, res: Response)=>  {
-    let id = req.params.id
-    let info = req.body;
-    res.send(await DriverService.update(id, info, driverSchema))
+    const { id } = req.params;
+    const { info } = req.body;
+    return DriverService.update(id, info, driverSchema)
+        .then(result => res.status(200).json(result))
+        .catch(error => 
+            error.statusCode ? res.status(error.statusCode).json(error) : 
+            res.status(500).json(Boom.internal('Internal Server Error').output.payload))
    
 };
 
 export let deleteDriver = async (req: Request, res: Response)=>  {
-    let id = req.params.id
-    res.send(await DriverService.del(id, driverSchema))
+    const { id } = req.params
+    return DriverService.del(id, driverSchema)
+    .then(result => res.status(200).json(result))
+    .catch(error => 
+        error.statusCode ? res.status(error.statusCode).json(error) : 
+        res.status(500).json(Boom.internal('Internal Server Error').output.payload))
    
 };
